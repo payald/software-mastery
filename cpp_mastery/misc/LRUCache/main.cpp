e768e1af43711f74698f20519dd146af9dbd26d4
Payal Deb <payal.deb@gmail.com>
2024-01-13 11:26:15 +0530

    LRUCache implementation using unordered map aka hash and linkedlist as queue

--- cpp_mastery/misc/LRUCache/main.cpp
+++ cpp_mastery/misc/LRUCache/main.cpp
@@ -42,7 +42,7 @@ public:
                 //check if it is the last entry or the only entry in the queue
                 return;
             }
-            //remove the node
+            //remove the node from its position
             if (node->prev!=nullptr){
                 node->prev->next=node->next;
                 if (qfront==node){
@@ -52,7 +52,7 @@ public:
             if (node->next!=nullptr){
                 node->next->prev=node->prev;
             }
-            //move it to head
+            //move it to head as it is the most recently used node
             node->next=qrear;
             node->prev=nullptr;
             qrear->prev=node;
@@ -60,13 +60,14 @@ public:
     }
     string get(int key){
         if (cacheMap.find(key)!=cacheMap.end()){
-            //entry is already present in hashmap
+            //entry is already present in cache
             Node *node=cacheMap[key];
+            //update the queue for the most recently used node
             updateQ(node);
             return (node->value);
         }
+        //entry not in cache
         return string("");
-        //throw "Data not cached yet";
 
     }
     int put(int key,string value){
@@ -74,7 +75,7 @@ public:
             return -1;
         }
         if (cacheMap.find(key)!=cacheMap.end()){
-            //entry is already present in hashmap
+            //entry is already present in cache
             Node* node=cacheMap[key];
             if ((node->value)!=value){
                 node->value=value;
@@ -89,39 +90,46 @@ public:
             //queue is full
             Node* tmp=qfront;
 
-            if (qfront==qrear){
-                qfront=qrear=node;
+            if (qfront!=qrear){
 
-            }else{
                 qfront=qfront->prev;//remove from front
                 qfront->next=nullptr;
-                qrear->prev=node;//add at the rear
-                node->next=qrear;
-                qrear=node;
 
             }
+            qrear->prev=node;//add at the rear
+            node->next=qrear;
+            qrear=node;
+
             cacheMap.erase(tmp->key);
             delete tmp;
 
         }else{
+
             if (qfront==qrear && qfront==nullptr){
+                //first entry in cache
                 qrear=qfront=node;
             }else{
-                qrear->prev=node;//add at the rear
                 node->next=qrear;
+                qrear->prev=node;//add at the rear
                 qrear=node;
             }
             listSize++;
         }
+        //insert into the cache map
         cacheMap.insert(pair<int,Node*>(key,node));
     }
 
     void display(){
         cout<<endl;
         Node *tmp=qrear;
+        cout<<endl<<"cache queue"<<endl;
         while(tmp!=nullptr){
             cout<<(tmp->value)<<" ";
             tmp=tmp->next;
+        }
+        cout<<endl<<"cache map"<<endl;
+        for(auto it=cacheMap.begin();it!=cacheMap.end();it++){
+            cout<<it->first<<" ";
         }
     }
 };
@@ -141,10 +149,13 @@ int main()
     cache->put(5,"efg");
     cache->put(6,"fgh");
     cache->display();
-
-    cout<<endl<<cache->get(4);
+    cache->put(1,"ooo");
+    cache->put(2,"mmm");
+    cache->display();
+    cout<<endl<<((cache->get(4))==""?"Empty":"Not empty");
     cout<<endl<<cache->get(6);
-    cout<<endl<<((cache->get(1)).empty()?"Empty":"Not empty");
+    cout<<endl<<((cache->get(1))==""?"Empty":"Not empty");
+    cache->display();
     }catch(exception &e){
         cout<<"\n caught an exception "<<e.what();
     }
