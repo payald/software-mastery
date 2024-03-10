3634afb303ac8ccad3bf2032e5807d9be0051f68
Payal Deb <payal.deb@gmail.com>
2024-03-10 11:49:00 +0530

    Interface and Implementation

---
+++ java_mastery/InterfaceImplementation/src/interfaceimplementation/Implementation.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package interfaceimplementation;
+
+/**
+ *
+ * @author payal
+ */
+public class Implementation implements ExtendedInterface {
+
+    @Override
+    public void extendedTerm() {
+        System.out.println("Extended term");
+    }
+
+    @Override
+    public void term1() {
+        System.out.println("Term 1");
+    }
+
+    @Override
+    public void term2() {
+        System.out.println("Term 2");
+    }
+    
+}
---
+++ java_mastery/InterfaceImplementation/src/interfaceimplementation/InterfaceImplementation.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package interfaceimplementation;
+
+/**
+ *
+ * @author payal
+ */
+public class InterfaceImplementation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        ExtendedInterface myinterface=new Implementation();
+        myinterface.extendedTerm();
+        myinterface.term1();
+        myinterface.term2();
+    }
+    
+}
